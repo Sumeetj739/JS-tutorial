3d0548b5675d0106cc12f4a4393798535426d80e
Sumeetj739 <sumeetj739@gmail.com>
2025-07-09 14:57:14 +0530

    data types

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,17 @@
+
+// we can not change the value of a constant variable
+const account_Id = 1234567890;
+// account_Id=2 
+console.log(account_Id);
+
+
+// it can cause issues in large code bases
+let accountEmail = "sumeet@gmail.com"  //let is block scoped
+accountCity = "Pune";
+//in javascript we define variables and not assign any values to is it is undefined
+//generally we use const and let we dont use var
+
+
+
+// we can change the value of a variable
+var accountPassword = "1234567890"  //scope of var is global dont use it because
---
+++ 02_data-types/data-types.js
@@ -0,0 +1,14 @@
+"use strict";  //treat all js code as newer version of js
+
+
+
+// data types in JS
+let name="sumeet"  //string ->""
+let age=25;       //number renge 2 to 53 /for above numbers use bigint
+let isLoggedIn=false  //boolean ->true /false
+console.log(typeof null)//null ->standalone value ->null type of is object
+console.log(typeof undefined);
+//symbol->unique
+
+
+//object
