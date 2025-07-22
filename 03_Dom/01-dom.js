1df08f17fb4db2f963049de0f0ac2c9c3cca78fe
Sumeetj739 <sumeetj739@gmail.com>
2025-07-22 10:13:23 +0530

    dom

---
+++ 03_Dom/01-dom.js
@@ -0,0 +1,44 @@
+// SELECTING DOM ELEMENT BY ID
+
+// document.getElementById('title')
+
+// document.getElementById('title').id
+
+// document.getElementById('title').className   GETTING THE NAME OF CLASS BY SELECTED ID
+
+
+
+//document.getElementById('title').className   with use of this we can set attribute
+
+
+// document.getElementById('title').setAttribute('class' ,'test heading')      used for setting attribute
+
+
+//applying style through dom
+
+// const title =document.getElementById('title')
+
+// const title = document.getElementById('title')
+
+
+// title.style.backgroundColor='pink'
+
+///---------for manipulatif text in dom
+
+
+// title.textContent
+// ' dom learning'
+// title.innerHTML
+// ' dom learning'
+// title.innerText
+// ' dom learning'
+
+
+
+// select element by class
+
+// document.getElementsByClassName('heading')
+
+
+//select element by query
+// document.querySelector('h1')
